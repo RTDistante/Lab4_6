58534691eeb56faeedab48b3f27a28d1a6ae87a6
RTDistante <66966970+RTDistante@users.noreply.github.com>
2020-07-06 12:33:18 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDay = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            List<string> taskList = new List<string>();
+
+            for (int i = 0; i < weekDay.Length; i++)
+            {
+                Console.WriteLine("Type in a new task for " + weekDay[i]);
+                string inputTask = Console.ReadLine();
+                taskList.Add(inputTask);
+            }
+
+            for (int i = 0; i < weekDay.Length; i++)
+            {
+                Console.WriteLine(weekDay[i] + ": " + taskList[i]);
+            }
+
+        }
+    }
+}
